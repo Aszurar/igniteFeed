a42d2b50606c735b9dd87e3c19aae36aa1ce4791
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-06-05 00:12:37 -0300

    ♻️ 💄 Refatoração da Home para display grid e atender ao layout do Figma 💄♻️

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,29 +1,48 @@
 import { Post } from '../../components/Post';
+import { Sidebar } from '../../components/Sidebar';
 
 import styles from './styles.module.css';
 
 export function Home() {
   return (
-    <div style={{ flex: 1 }}>
-      {/* <div
-        style={{
-          backgroundColor: '#e57373',
-          borderRadius: 5,
-          display: 'flex',
-          height: 100,
-          width: 100,
-        }}
-      />*/}
-      <div style={styles.containerum} />
-      <div style={styles.containerdois} />
-      <Post
-        author="Lucas de Lima"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facere provident quod eius, consectetur, sint dolor voluptates autem iste est repudiandae dolores delectus neque aliquam deserunt nam nesciunt a asperiores."
-      />
-      <Post
-        author="Sorveteiro"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facere provident quod eius, consectetur, sint dolor voluptates autem iste est repudiandae dolores delectus neque aliquam deserunt nam nesciunt a asperiores."
-      />
+    <div>
+      <div className={styles.container}>
+        <Sidebar />
+        <div className={styles.squareBlue}>
+          <Post
+            author="Lucas de Lima"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facere provident quod eius, consectetur, sint dolor voluptates autem iste est repudiandae dolores delectus neque aliquam deserunt nam nesciunt a asperiores."
+          />
+          <Post
+            author="Sorveteiro"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facere provident quod eius, consectetur, sint dolor voluptates autem iste est repudiandae dolores delectus neque aliquam deserunt nam nesciunt a asperiores."
+          />
+          <Post
+            author="Sorveteiro"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facere provident quod eius, consectetur, sint dolor voluptates autem iste est repudiandae dolores delectus neque aliquam deserunt nam nesciunt a asperiores."
+          />
+          <Post
+            author="Sorveteiro"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facere provident quod eius, consectetur, sint dolor voluptates autem iste est repudiandae dolores delectus neque aliquam deserunt nam nesciunt a asperiores."
+          />
+          <Post
+            author="Sorveteiro"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facere provident quod eius, consectetur, sint dolor voluptates autem iste est repudiandae dolores delectus neque aliquam deserunt nam nesciunt a asperiores."
+          />
+          <Post
+            author="Sorveteiro"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facere provident quod eius, consectetur, sint dolor voluptates autem iste est repudiandae dolores delectus neque aliquam deserunt nam nesciunt a asperiores."
+          />
+          <Post
+            author="Sorveteiro"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facere provident quod eius, consectetur, sint dolor voluptates autem iste est repudiandae dolores delectus neque aliquam deserunt nam nesciunt a asperiores."
+          />
+          <Post
+            author="Sorveteiro"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facere provident quod eius, consectetur, sint dolor voluptates autem iste est repudiandae dolores delectus neque aliquam deserunt nam nesciunt a asperiores."
+          />
+        </div>
+      </div>
     </div>
   );
 }
