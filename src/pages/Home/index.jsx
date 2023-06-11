1fb176e5d1f1bf1580952b12cabba46edcc95a28
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-06-10 22:09:31 -0300

    ♻️Atualziação do uso do componente de Post ♻️

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -8,39 +8,11 @@ export function Home() {
     <div>
       <div className={styles.container}>
         <Sidebar />
-        <div className={styles.squareBlue}>
-          <Post
-            author="Lucas de Lima"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facere provident quod eius, consectetur, sint dolor voluptates autem iste est repudiandae dolores delectus neque aliquam deserunt nam nesciunt a asperiores."
-          />
-          <Post
-            author="Sorveteiro"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facere provident quod eius, consectetur, sint dolor voluptates autem iste est repudiandae dolores delectus neque aliquam deserunt nam nesciunt a asperiores."
-          />
-          <Post
-            author="Sorveteiro"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facere provident quod eius, consectetur, sint dolor voluptates autem iste est repudiandae dolores delectus neque aliquam deserunt nam nesciunt a asperiores."
-          />
-          <Post
-            author="Sorveteiro"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facere provident quod eius, consectetur, sint dolor voluptates autem iste est repudiandae dolores delectus neque aliquam deserunt nam nesciunt a asperiores."
-          />
-          <Post
-            author="Sorveteiro"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facere provident quod eius, consectetur, sint dolor voluptates autem iste est repudiandae dolores delectus neque aliquam deserunt nam nesciunt a asperiores."
-          />
-          <Post
-            author="Sorveteiro"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facere provident quod eius, consectetur, sint dolor voluptates autem iste est repudiandae dolores delectus neque aliquam deserunt nam nesciunt a asperiores."
-          />
-          <Post
-            author="Sorveteiro"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facere provident quod eius, consectetur, sint dolor voluptates autem iste est repudiandae dolores delectus neque aliquam deserunt nam nesciunt a asperiores."
-          />
-          <Post
-            author="Sorveteiro"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, facere provident quod eius, consectetur, sint dolor voluptates autem iste est repudiandae dolores delectus neque aliquam deserunt nam nesciunt a asperiores."
-          />
+        <div>
+          <Post author="Lucas de Lima" />
+          <Post author="Sorveteiro" />
+          <Post author="Sorveteiro" />
+          <Post author="Sorveteiro" />
         </div>
       </div>
     </div>
