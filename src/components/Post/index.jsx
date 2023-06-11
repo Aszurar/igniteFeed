0722708ca24c4631ea0f9026101c7ec870f7eff3
Lucas De Lima Martins De Souza <lms.souza39@gmail.com>
2023-06-10 22:07:15 -0300

    ✨Criação do componente de Post✨

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,16 +1,56 @@
 import PropTypes from 'prop-types';
 
-export function Post({ author, content }) {
-  console.log('teste');
+import styles from './styles.module.css';
+import { Avatar } from '../avatar';
+import { Divisor } from '../Divisor';
+export function Post({ author }) {
   return (
-    <>
-      <strong>{author}</strong>
-      <p>{content}</p>
-    </>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <Avatar urlImage="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" />
+
+          <div className={styles.authorInfo}>
+            <strong>{author}</strong>
+            <p>Dev Front-End</p>
+          </div>
+        </div>
+
+        <time title="11 de Maio às 8:13h" dateTime="2022-05-11:08:13:30">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <main>
+        <p>Fala galeraa 👋</p>
+
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O
+          nome do projeto é DoctorCare 🚀
+        </p>
+
+        <p className={styles.link}>
+          <a href="#">👉 jane.design/doctorcare</a>
+        </p>
+
+        <p className={styles.link}>
+          <a href="#">#novoprojeto</a>
+          <a href="#">#nlw</a>
+          <a href="#">#rocketseat</a>
+        </p>
+      </main>
+
+      <Divisor />
+
+      <form className={styles.commentForm}>
+        <h5>Deixe seu feedback</h5>
+        <textarea placeholder="Escreva um comentário..." />
+        <button type="submit">Comentar</button>
+      </form>
+    </article>
   );
 }
 
 Post.propTypes = {
   author: PropTypes.string.isRequired,
-  content: PropTypes.string.isRequired,
 };
