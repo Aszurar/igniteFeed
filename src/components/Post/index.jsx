e787d7ba63f3dae96255d7fe0c23546b40309787
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-08-30 10:00:58 -0300

    :sparkles: Exibição dos posts e das datas com o tratamento das datas pelo date-fns e lógica de Estado para os comentários :sparkles:

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -4,92 +4,93 @@ import styles from './styles.module.css';
 import { Avatar } from '../Avatar';
 import { Divisor } from '../Divisor';
 import { Comment } from '../Comment';
-export function Post({ author }) {
+import { format, formatDistanceToNow } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
+import { useState } from 'react';
+export function Post({ author, publishedAt, content }) {
+  const publishedDateToISO = publishedAt.toISOString();
+  const publishedDateFormatted = format(publishedAt, "dd 'de' MMMM 'às' HH:mm'h'", { locale: ptBR });
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, { locale: ptBR, addSuffix: true });
+
+  const [comments, setComments] = useState([
+    "Post maneiro demais!",
+  ]);
+  const [newComment, setNewComment] = useState('');
+
+  const contentList = content.map((line, index) => {
+    if (line.type === 'paragraph') {
+      return <p key={index}>{line.string}</p>
+    }
+    if (line.type === 'link') {
+      return <p key={index} className={styles.link}><a href="#">{line.string}</a></p>
+    }
+  });
+  const commentsList = comments.map((comment, index) => {
+    return <Comment
+      key={index}
+      like={33}
+      content={comment}
+      name='Jenny Wilson'
+      avatar='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1061&q=80'
+    />
+  });
+
+  function handleUpdateNewComment(event) {
+    setNewComment(event.target.value);
+  }
+
+  function handleAddNewComment(event) {
+    event.preventDefault();
+
+    if (newComment.trim() === '') {
+      return;
+    }
+
+    setComments([...comments, newComment]);
+    setNewComment('');
+  }
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar urlImage="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" />
+          <Avatar urlImage={author.avatarUrl} />
 
           <div className={styles.authorInfo}>
-            <strong>{author}</strong>
-            <p>Dev Front-End</p>
+            <strong>{author.name}</strong>
+            <p>{author.role}</p>
           </div>
         </div>
-
-        <time title="11 de Maio às 8:13h" dateTime="2022-05-11:08:13:30">
-          Publicado há 1h
+        <time title={publishedDateFormatted} dateTime={publishedDateToISO}>
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <main>
-        <p>Fala galeraa 👋</p>
-
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O
-          nome do projeto é DoctorCare 🚀
-        </p>
-
-        <p className={styles.link}>
-          <a href="#">👉 jane.design/doctorcare</a>
-        </p>
-
-        <p className={styles.link}>
-          <a href="#">#novoprojeto</a>
-          <a href="#">#nlw</a>
-          <a href="#">#rocketseat</a>
-        </p>
+        {contentList}
       </main>
 
       <Divisor />
 
-      <form className={styles.commentForm}>
+      <form className={styles.commentForm} onSubmit={handleAddNewComment}>
         <h5>Deixe seu feedback</h5>
-        <textarea placeholder="Escreva um comentário..." />
+        <textarea
+          placeholder="Escreva um comentário..."
+          value={newComment}
+          onChange={handleUpdateNewComment}
+        />
         <button type="submit">Comentar</button>
       </form>
 
       <div className={styles.commentList}>
-        <Comment
-          time='1'
-          like={33}
-          name='Jenny Wilson'
-          content='Adorei seu novo portfolio!'
-          avatar='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1061&q=80' />
-
-        <Comment
-          time='1'
-          like={33}
-          name='Jenny Wilson'
-          content='Adorei seu novo portfolio!'
-          avatar='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1061&q=80' />
-
-        <Comment
-          time='1'
-          like={33}
-          name='Jenny Wilson'
-          content='Adorei seu novo portfolio!'
-          avatar='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1061&q=80' />
-
-        <Comment
-          time='1'
-          like={33}
-          name='Jenny Wilson'
-          content='Adorei seu novo portfolio!'
-          avatar='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1061&q=80' />
-
-        <Comment
-          time='1'
-          like={33}
-          name='Jenny Wilson'
-          content='Adorei seu novo portfolio!'
-          avatar='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1061&q=80' />
-
+        {commentsList}
       </div>
     </article>
   );
 }
 
 Post.propTypes = {
-  author: PropTypes.string.isRequired,
+  author: PropTypes.object.isRequired,
+  publishedAt: PropTypes.instanceOf(Date).isRequired,
+  content: PropTypes.array.isRequired,
 };
