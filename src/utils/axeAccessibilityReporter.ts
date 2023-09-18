6bd8858df04a59270ba045fce940ed4bf545a14a
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-09-18 01:29:44 -0300

    :wheelchair: Criação da função do axe-core :wheelchair:

---
+++ src/utils/axeAccessibilityReporter.ts
@@ -0,0 +1,11 @@
+
+export async function axeAccessibilityReporter() {
+
+  if (process.env.NODE_ENV === 'development') {
+    const axe = await require('axe-core');
+    const React = await require('react');
+    const ReactDOM = await require('react-dom');
+
+    axe(React, ReactDOM, 1000);
+  }
+}
