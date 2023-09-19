94ad1a05fa80e76d664498a3ee8a28b2af2a540a
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-09-19 16:12:39 -0300

    :wheelchair: Adição do axe-core :wheelchair:

--- src/utils/axeAccessibilityReporter.ts
+++ src/utils/axeAccessibilityReporter.ts
@@ -1,11 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 export async function axeAccessibilityReporter() {
+  if (import.meta.env.DEV) {
+    const axe = await import('@axe-core/react');
 
-  if (process.env.NODE_ENV === 'development') {
-    const axe = await require('axe-core');
-    const React = await require('react');
-    const ReactDOM = await require('react-dom');
-
-    axe(React, ReactDOM, 1000);
+    axe.default(React, ReactDOM, 1000);
   }
 }
