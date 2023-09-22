dc08b52c7d45e7d74c79cf98e0785c2e549380a3
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-09-22 00:41:50 -0300

    :goal_net::heavy_plus_sign: Adição do Sentry :heavy_plus_sign::goal_net:

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { Header } from './components/Header';
 import { Home } from './pages/Home';
+import * as Sentry from "@sentry/react";
 
 import './theme/global.css';
 import { axeAccessibilityReporter } from './utils/axeAccessibilityReporter';
 
 axeAccessibilityReporter()
+
+Sentry.init({
+  dsn: "https://1ca690c32dab081b029acee254f1dec0@o4505921743552512.ingest.sentry.io/4505921746698240",
+  integrations: [new Sentry.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+  // Set `tracePropagationTargets` to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/ignitefeed-aszurar.netlify.app\//],
+});
 
 function App() {
   return (
